6242f50ac0cb00160da865cc27c85a5c842689d9
ahmed <a.elmahdi13@gmail.com>
2025-08-30 16:22:19 +0300

    netlify3

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,6 @@
 import serverless from 'serverless-http';
 import app from '../../src/app.js';
 
-export const handler = serverless(app, {
-  basePath: '/api'
-});
+// Do NOT set basePath here. Our Express app already mounts routes with /api/*.
+export const handler = serverless(app);
 
